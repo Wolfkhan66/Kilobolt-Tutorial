6e019a6a4555e7f392dc5a0b82e73cfd0148cf43
Wolfkhan66 <cailehwald@googlemail.com>
2015-12-07 22:31:31 +0000

    Basic AI

--- kilobolt tutorial/src/kiloboltgame/Enemy.java
+++ kilobolt tutorial/src/kiloboltgame/Enemy.java
@@ -4,32 +4,56 @@ import java.awt.Rectangle;
 
 public class Enemy {
 
-	private int power, speedX, centerX, centerY;
+	private int power, centerX, speedX, centerY;
 	private Background bg = StartingClass.getBg1();
+	private Robot robot = StartingClass.getRobot();
 
-	public Rectangle r = new Rectangle(0,0,0,0);
+	public Rectangle r = new Rectangle(0, 0, 0, 0);
 	public int health = 5;
+
+	private int movementSpeed;
 
 	// Behavioral Methods
 	public void update() {
+		follow();
 		centerX += speedX;
-		speedX = bg.getSpeedX()*5;
-		r.setBounds(centerX - 25, centerY-25, 50, 60);
-		
-		if (r.intersects(Robot.yellowRed)){
+		speedX = bg.getSpeedX() * 5 + movementSpeed;
+		r.setBounds(centerX - 25, centerY - 25, 50, 60);
+
+		if (r.intersects(Robot.yellowRed)) {
 			checkCollision();
 		}
-		
+
 	}
 
-
-
 	private void checkCollision() {
-		if (r.intersects(Robot.rect) || r.intersects(Robot.rect2) || r.intersects(Robot.rect3) || r.intersects(Robot.rect4)){
+		if (r.intersects(Robot.rect) || r.intersects(Robot.rect2)
+				|| r.intersects(Robot.rect3) || r.intersects(Robot.rect4)) {
 			System.out.println("collision");
-			
+
+		}
+	}
+
+	public void follow() {
+		
+		if (centerX < -95 || centerX > 810){
+			movementSpeed = 0;
+		}
+
+		else if (Math.abs(robot.getCenterX() - centerX) < 5) {
+			movementSpeed = 0;
+		}
+
+		else {
+
+			if (robot.getCenterX() >= centerX) {
+				movementSpeed = 1;
+			} else {
+				movementSpeed = -1;
 			}
 		}
+
+	}
 
 	public void die() {
 
@@ -59,8 +83,6 @@ public class Enemy {
 		return bg;
 	}
 
-
-
 	public void setPower(int power) {
 		this.power = power;
 	}
@@ -81,5 +103,4 @@ public class Enemy {
 		this.bg = bg;
 	}
 
-	
 }
